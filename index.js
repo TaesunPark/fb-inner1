201c7184710c2f7060121a13f510749af5c0b018
TaesunPark <59998914+TaesunPark@users.noreply.github.com>
2020-02-23 19:14:28 +0900

    facebook_demo
    수정중

---
+++ index.js
@@ -0,0 +1,28 @@
+
+function yfunction(){
+    var i;
+    var strYear = "";
+    for(i=2020;i>=1905;i--)
+    {
+        strYear += "<option value="+i+">"+i+"</option>";
+    }
+    document.getElementById("drop1").innerHTML = strYear;
+}
+function mfunction(){
+    var i;
+    var strMonth = "";
+    for(i=1;i<=12;i++)
+    {
+        strMonth += "<option value="+i+">"+i+"월"+"</option>";
+    }
+    document.getElementById("drop2").innerHTML = strMonth;
+}
+function dfunction(){
+    var i;
+    var strDay = "";
+    for(i=1;i<=31;i++)
+    {
+        strDay += "<option value="+i+">"+i+"</option>";
+    }
+    document.getElementById("drop3").innerHTML = strDay;
+}
